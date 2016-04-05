644b23244368b86421b2259787cc716c3ab4784f
Bomberus <maximilian@cdsffo.de>
2016-04-05 22:55:48 +0200

    Secound try to push this branch into the repo

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,8 +1,8 @@
 <html>
 <head>
     <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="{{ URL::asset('css/materialize.min.css') }}"  media="screen,projection"/>
-    <link type="text/css" rel="stylesheet" href="{{ URL::asset('css/mainpage/mainpage.css') }}"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="{{ URL::asset('css/materialize.min.css') }} "  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="{{ URL::asset('css/mainpage/mainpage.css') }} "  media="screen,projection"/>
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
